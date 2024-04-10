af48e4ad11d0017061bacacbf139df6112b4d1b0
mallusrgreat <mallusrgreat@duck.com>
2024-04-10 15:15:23 +0530

    chore: remove interaction file name suffixes

--- src/interactions/buttons/general/example.button.ts
+++
@@ -1,8 +0,0 @@
-import Button from "@/structures/button.js";
-
-export default new Button({
-	customId: "example",
-	async run(client, interaction) {
-		await interaction.reply("Hello!");
-	},
-});
--- src/interactions/modals/general/example.modal.ts
+++
@@ -1,8 +0,0 @@
-import Modal from "@/structures/modal.js";
-
-export default new Modal({
-	customId: "example",
-	async run(client, interaction) {
-		await interaction.reply("Hello!");
-	},
-});
